f40a0d60bb227dc42d6516a85321559f2e0bba13
Tyler <tyler.james.downey@gmail.com>
2019-01-13 13:51:05 -0330

    Created initial graphical frame with rectangularm hives and buttons

--- HiveComponents.java
+++ HiveComponents.java
@@ -1,28 +1,40 @@
-package view;
-
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.util.ArrayList;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JComponent;
-
-public class HiveComponents extends JComponent{
+public class HiveComponents extends JPanel{
+		private int myTimerDelay;
+		private final Timer myTimer;
 		private ArrayList<Rectangle> hives = new ArrayList<Rectangle>();
 		public HiveComponents() {
+			super();
+			myTimerDelay = 1000;
+			myTimer = new Timer(myTimerDelay, hiveTimer);
+			myTimer.start();
+			setBackground(new Color(0,153,0));
 		}
 		public void paintComponent(Graphics g) {
-			addHive();
-			addHive();
 			Graphics2D g2 = (Graphics2D) g;
+			super.paintComponent(g2);
 			g2.setColor(Color.YELLOW);
-			g2.setBackground(Color.BLACK);
 			for (Rectangle element : hives) {
 				g2.fill(element);
 			}
 		}
 		public void addHive() {
-			hives.add(new Rectangle(getWidth() - 100, 100 * hives.size(), 80, 80));
+			hives.add(new Rectangle(20, 100 * hives.size(), 80, 80));
 		}
+		ActionListener hiveTimer = new ActionListener() {
+		@Override
+			public void actionPerformed(ActionEvent theEvent) {
+				addHive();
+				repaint();
+		}
+	};
 }
--- HiveViewer.java
+++ HiveViewer.java
@@ -1,6 +1,5 @@
-package view;
-
 import javax.swing.JFrame;
+import javax.swing.JButton;
 
 public class HiveViewer {
 	public static final int FRAME_WIDTH = 600;
@@ -9,6 +8,16 @@ public class HiveViewer {
 		HiveComponents hc = new HiveComponents();
 		JFrame frame = new JFrame();
 		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+
+		JButton sellButton = new JButton("Sell");
+		hc.add(sellButton);
+		JButton buyButton = new JButton("Buy Upgrades");
+		hc.add(buyButton);
+		JButton splitButton = new JButton("Split Hives");
+		hc.add(splitButton);
+		JButton sqButton = new JButton("Save and Quit");
+		hc.add(sqButton);
+
 		frame.add(hc);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
